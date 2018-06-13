23ca815a371cec6a26ed8b95744ca74174c7c258
Evan Kerr <evankrkerr90@gmail.com>
2018-06-13 22:09:55 +0000

    reordered server file for better readability

--- express_server.js
+++ express_server.js
@@ -2,16 +2,23 @@ const express = require("express");
 const app = express();
 const PORT = 8080; // default port 8080
 const bodyParser = require("body-parser");
+const cookieParser = require('cookie-parser')
+
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(cookieParser());
+
+app.set("view engine", "ejs");
+
+//sets what port to receive requests from
+app.listen(PORT, () => {
+  console.log(`Example app listening on port ${PORT}!`);
+});
 
 function generateRandomString() {
   return Math.random().toString(36).substring(2, 5) + Math.random().toString(36).substring(2, 5);
 };
 
-app.use(bodyParser.urlencoded({extended: true}));
-
-app.set("view engine", "ejs");
-
-var urlDatabase = {
+const urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
   "9sm5xK": "http://www.google.com"
 };
@@ -19,44 +26,33 @@ var urlDatabase = {
 app.get("/", (req, res) => {
   res.end("Hello!");
 });
-//deletes the associated short and long URLs when the user
-//clicks the submit button
-app.post("/urls/:id/delete", (req, res) => {
-delete urlDatabase[req.params.id];
-//console.log(req.params.id);
-res.redirect("/urls")
-});
+
+
 //renders the urls_index html file
 app.get("/urls", (req, res) => {
-  let urlsIndex = {urls: urlDatabase};
+  let urlsIndex = {
+  username: req.cookies["username"],
+  urls: urlDatabase
+};
   res.render('urls_index', urlsIndex);
 });
 
-//adds the generated short URL and the associated
-//long URL to the database object
-app.post("/urls", (req, res) => {
-  //console.log(req.body);
-  let shortURL = generateRandomString()  // debug statement to see POST parameters
-  urlDatabase[shortURL] = req.body['longURL'];
-  //res.send("Ok");
-  res.redirect('/urls/' + shortURL)    // Respond with 'Ok' (we will replace this)
-});
+
 //displays the page where the user enters a URL to shorten
 app.get("/urls/new", (req, res) => {
   res.render("urls_new");
 });
 
-app.post("/urls/:id", (req, res) => {
-  urlDatabase[req.params.id] = req.body.newURL;
-  //console.log(req)
-  res.redirect("/urls");
-});
 //generates the page that the user is redirected to when they
 //submit a url to shorten
 app.get("/urls/:id", (req, res) => {
   let longURL = urlDatabase[req.params.id];
   //console.log(longURL)
-  let urlsShow = { shortURL: req.params.id, longURL: longURL };
+  let urlsShow = {
+    shortURL: req.params.id,
+    longURL: longURL,
+    username: req.cookies["username"]
+  };
   res.render('urls_show', urlsShow);
 });
 //redirects the user to the long URL associated with the short URL
@@ -72,7 +68,36 @@ app.get("/urls.json", (req, res) => {
 //app.get("/hello", (req, res) => {
   //res.end("<html><body>Hello <b>World</b></body></html>\n")
 //});
-//sets what port to receive requests from
-app.listen(PORT, () => {
-  console.log(`Example app listening on port ${PORT}!`);
+
+//adds the generated short URL and the associated
+//long URL to the database object
+app.post("/urls", (req, res) => {
+  //console.log(req.body);
+  let shortURL = generateRandomString()  // debug statement to see POST parameters
+  urlDatabase[shortURL] = req.body['longURL'];
+  //res.send("Ok");
+  res.redirect('/urls/' + shortURL)    // Respond with 'Ok' (we will replace this)
 });
+
+//deletes the associated short and long URLs when the user
+//clicks the submit button
+app.post("/urls/:id/delete", (req, res) => {
+delete urlDatabase[req.params.id];
+res.redirect("/urls")
+});
+
+app.post("/urls/:id", (req, res) => {
+  urlDatabase[req.params.id] = req.body.newURL;
+  //console.log(req)
+  res.redirect("/urls");
+});
+
+app.post("/login", (req, res) => {
+username = req.body.username;
+res.cookie('username', username);
+res.redirect('/urls');
+});
+
+
+
+
